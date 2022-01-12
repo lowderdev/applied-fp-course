b498f9695b54e5c48ab7251e1f8cfadf193b3fde
Logan Lowder <logan.lowder@logikcull.com>
2022-01-12 16:27:33 -0600

    Complete level 01

--- src/Level01/Core.hs
+++ src/Level01/Core.hs
@@ -32,8 +32,13 @@ app
   :: Request
   -> (Response -> IO ResponseReceived)
   -> IO ResponseReceived
-app _ cb =
-  error "Application not implemented"
+app _ respond = do
+  putStrLn "I've done some IO here"
+  respond $
+    responseLBS
+      status200
+      [("Content-Type", "text/plain")]
+      "Hello, Web!"
 
 -- We keep this main function here as it is useful to build your application as
 -- a library. The reasoning behind this is that when you come to do your
@@ -41,4 +46,6 @@ app _ cb =
 -- needing to worry about any initialisation code you've buried in your
 -- executable Main.hs.
 runApp :: IO ()
-runApp = run undefined undefined
+runApp = do
+  putStrLn "http://localhost:8090/"
+  run 8090 app
